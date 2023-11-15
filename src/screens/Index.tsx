b4bd0660fc2c736a599f3a1424f6a957fbd11b51
Connor Cain <connor@sahha.ai>
2023-11-15 14:25:59 +1300

    Sahha authentication and deauthentication set up.

--- src/screens/Index.tsx
+++ src/screens/Index.tsx
@@ -1,11 +1,63 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
 import globalStyles from '../global-styles';
+import Sahha from 'sahha-react-native';
+
+const appId = '';
+const appSecret = '';
+const externalId = 'test-external-id';
 
 function Index(): JSX.Element {
+  const [authentication, setAuthentication] = useState({
+    loading: false,
+    authenticated: false,
+  });
+
+  const authenticateSahha = () => {
+    setAuthentication({ ...authentication, loading: true });
+
+    Sahha.authenticate(appId, appSecret, externalId, (error, success) => {
+      console.log(`Sahha authentication success: ${success}`);
+      setAuthentication({ loading: false, authenticated: success });
+
+      if (error) {
+        console.log(`Sahha authentication error: ${error}`);
+      }
+    });
+  };
+
+  const deauthenticateSahha = () => {
+    setAuthentication({ ...authentication, loading: true });
+
+    Sahha.deauthenticate((error, success) => {
+      console.log(`Sahha deauthentication success: ${success}`);
+      setAuthentication({ loading: false, authenticated: !success });
+
+      if (error) {
+        console.log(`Sahha deauthentication error: ${error}`);
+      }
+    });
+  };
+
   return (
     <View style={globalStyles.container}>
-      <Text>Index screen</Text>
+      <View style={{ gap: 12 }}>
+        <Text>
+          {authentication.loading
+            ? 'Authenticating...'
+            : `Authenticated: ${authentication.authenticated}`}
+        </Text>
+        <Button
+          title="Authenticate Sahha"
+          onPress={authenticateSahha}
+          disabled={authentication.loading}
+        />
+        <Button
+          title="Deauthenticate Sahha"
+          onPress={deauthenticateSahha}
+          disabled={authentication.loading}
+        />
+      </View>
     </View>
   );
 }
