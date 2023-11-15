e0fe41e6ef863c70d77e7b247c73e66d2ee26a64
Connor Cain <connor@sahha.ai>
2023-11-15 13:50:24 +1300

    Install sahha SDK and configure SDK on app launch.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import {
@@ -6,6 +6,7 @@ import {
   createNativeStackNavigator,
 } from '@react-navigation/native-stack';
 import Index from './screens/Index';
+import Sahha, { SahhaEnvironment, SahhaSensor } from 'sahha-react-native';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,10 +15,31 @@ const screenOptions: NativeStackNavigationOptions = {
   headerBackTitleVisible: false,
 };
 
+const sahhaSettings = {
+  environment: SahhaEnvironment.sandbox, // Required -  .sandbox for testing
+  // Optional - Android only
+  // notificationSettings: {
+  //   icon: 'ic_test',
+  //   title: 'Test Title',
+  //   shortDescription: 'Test description.',
+  // },
+  sensors: [SahhaSensor.pedometer], // Optional - defaults to all sensors
+};
+
 function App(): JSX.Element {
+  useEffect(() => {
+    Sahha.configure(sahhaSettings, (error, success) => {
+      console.log(`Sahha configuration success: ${success}`);
+
+      if (error) {
+        console.log(`Sahha configuration error: ${error}`);
+      }
+    });
+  }, []);
+
   return (
     <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
       <Stack.Navigator screenOptions={screenOptions}>
         <Stack.Screen
           name="Index"
